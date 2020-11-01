4d80e2099464ae3bc443140a97cdf6701aa0695a
Rushil Baya <rushil.baya@gmail.com>
2020-11-01 12:33:13 +0800

    added subject generator

--- student.py
+++ student.py
@@ -1,13 +1,15 @@
+from subjgen import *
+
 class Student:
     def __init__(self, name, gender, dob, subjects = []):
         self.name = name
         self.gender = gender
         self.dob = dob
-        self.subjects = subjects if len(subjects) == 4 else 
+        self.subjects = subjects if check_subjects(subjects) else choose_subjects()
         
     def __str__(self):
         return f'{self.name}: {self.gender}, {self.dob}, {self.subjects}'
         
     
-a = Student('rushil', 'M', '12MAY06', ['geo', 'cs', 'french', 'drama'])
+a = Student('rushil', 'M', '12MAY06', [])
 print(a)
--- subjgen.py
+++ subjgen.py
@@ -1,19 +1,32 @@
 from random import choice
-subjects = {'humanities': ['geography', 'history', 'economics', 'computerscience', 'psychology', 'politics', 'business',
- 'environmental'], 'arts': ['music', 'art', 'film', 'drama', 'dt'], 'language': ['chinese', 'spanish', 'french', 'german']}
+subjects = {'sciences': ['biology', 'physics', 'chemistry'], 'humanities': ['geography', 'history', 'economics', 'computerscience', 'psychology', 
+'politics', 'business', 'environmental'], 'arts': ['music', 'art', 'film', 'drama', 'dt'], 'languages': ['chinese', 'spanish', 'french', 'german']}
 
 def choose_subjects():
-    chosen = []
+    chosen = ['english', 'maths']
     for k, v in subjects.items():
-        if k == 'humanities':
-            hums1 = choice(v)
-            chosen.append(hums1)
-            hums2 = choice(v)
-            while hums2 == hums1:
-                hums2 = choice(v)
-            chosen.append(hums2)
+        if k == 'sciences' or k == 'humanities':
+            sub1 = choice(v)
+            chosen.append(sub1)
+            sub2 = choice(v)
+            while sub2 == sub1:
+                sub2 = choice(v)
+            chosen.append(sub2)
         else:
             chosen.append(choice(v))
     return chosen
-print(choose_subjects())
+
+def check_subjects(chosen):
+    if len(chosen) == 8:
+        if chosen[0] == 'english':
+            if chosen[1] == 'maths':
+                if chosen[2] in subjects['sciences'] and chosen[3] in subjects['sciences']:
+                    if chosen[4] in subjects['humanities'] and chosen[5] in subjects['humanities']:
+                        if chosen[6] in subjects['arts']:
+                            if chosen[7] in subjects['languages']:
+                                return True
+    return False
+
+if __name__ == '__main__':
+    print(choose_subjects())
         
